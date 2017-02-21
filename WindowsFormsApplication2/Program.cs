7abd1cc27186d0c07c23e4ded2a9eaa11aafe27c
thatsgerman <jofouts@gmail.com>
2017-02-21 08:41:01 -0700

    weird

--- WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Example1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace WindowsFormsApplication2
 {
